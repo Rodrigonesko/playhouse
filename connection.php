488cf3d0b6b5bed1eb54dace4aca332291058f91
Rodrigo Onesko Dias <rodrigo_onesko@hotmail.com>
2022-08-15 13:30:44 -0300

    installing .env
    Installing .enc

--- connection.php
+++ connection.php
@@ -1,11 +1,14 @@
 <?php
 
-    $user = 'root';
-    $password = '';
-    $database = 'playhouse';
-    $host = 'localhost';
+    require_once 'global.php';
 
-    $mysqli = new mysqli($host, $user, $password, $database);
+    $user = $_ENV['USER_DB'];
+    $password = $_ENV['PASS_DB'];
+    $database = $_ENV['NAME_DB'];
+    $host = $_ENV['HOST_DB'];
+    $port = '3307';
+
+    $mysqli = new mysqli($host, $user, $password, $database, $port);
 
     if($mysqli->error){
         die("Falha ao conectar com o banco de dados");
